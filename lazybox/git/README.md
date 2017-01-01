b564266851b468b64f7c259b206987e781b633d5
h <h@h.h>
2017-01-01 21:32:55 +0800

    init

---
+++ lazybox/git/README.md
@@ -0,0 +1,4 @@
+GIT configuration file
+======================
+
+
---
+++ lazybox/git/git-cfg.sh
@@ -0,0 +1,21 @@
+# Color
+
+git config --global color.interactive auto  
+git config --global color.grep auto  
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
+git config --global color.ui auto
+
+# Identity
+
+git config --global user.name "h"
+git config --global user.email h@h.h
+
+# Editor
+
+git config --global core.editor vim
+
+# Mode Checking
+
+#git config --global core.filemode false                                                       
